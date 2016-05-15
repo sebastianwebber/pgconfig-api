0f0bbd0cffbe7b2c76b4b07980c94beb0bafa66e
Sebastian Webber <sebastian@swebber.me>
2016-05-14 21:05:54 -0300

    finishing doc stuff - for real

--- docs/conf.py
+++ docs/conf.py
@@ -1,8 +1,7 @@
 # -*- coding: utf-8 -*-
-<<<<<<< HEAD
 #
 # PGConfig API documentation build configuration file, created by
-# sphinx-quickstart on Sat May 14 17:30:16 2016.
+# sphinx-quickstart on Sat May 14 21:01:34 2016.
 #
 # This file is execfile()d with the current directory set to its
 # containing dir.
@@ -33,9 +32,9 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.doctest',
     'sphinx.ext.todo',
+    'sphinx.ext.coverage',
     'sphinx.ext.imgmath',
     'sphinx.ext.ifconfig',
-    'sphinx.ext.viewcode',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -48,33 +47,12 @@ source_suffix = '.rst'
 
 # The encoding of source files.
 #source_encoding = 'utf-8-sig'
-=======
-
-# At the bottom of conf.py
-def setup(app):
-    app.add_config_value('recommonmark_config', {
-            'url_resolver': lambda url: github_doc_root + url,
-            'auto_toc_tree_section': 'Contents',
-            }, True)
-    app.add_transform(AutoStructify)
-
-import recommonmark
-from recommonmark.transform import AutoStructify
-from recommonmark.parser import CommonMarkParser
-
-source_parsers = {
-    '.md': CommonMarkParser,
-}
-
-source_suffix = ['.rst', '.md']
->>>>>>> 2da6a8ca49859970d107b018edc500bc852517bf
 
 # The master toctree document.
 master_doc = 'index'
 
 # General information about the project.
 project = u'PGConfig API'
-<<<<<<< HEAD
 copyright = u'2016, Sebastian Webber'
 author = u'Sebastian Webber'
 
@@ -142,6 +120,7 @@ import sphinx_rtd_theme
 html_theme = "sphinx_rtd_theme"
 
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+github_doc_root = 'https://github.com/sebastianwebber/pgconfig-api/tree/master/docs'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -387,9 +366,3 @@ epub_exclude_files = ['search.html']
 
 # If false, no index is generated.
 #epub_use_index = True
-=======
-copyright = u'2016, Sebastian Webber and contributors'
-author = u'Sebastian Webber and contributors'
-
-github_doc_root = 'https://github.com/sebastianwebber/pgconfig-api/tree/master/docs'
->>>>>>> 2da6a8ca49859970d107b018edc500bc852517bf
