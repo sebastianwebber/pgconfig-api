327898b06b2ddf2ff45ff1b5ebac976b54567811
Sebastian Webber <sebastian@swebber.me>
2016-08-30 14:25:58 -0300

    fixing openshift startup file

--- app.py
+++ app.py
@@ -7,20 +7,21 @@
 # whatever framework is desired - or a completely customized service.
 #
 
-import imp
+import tornado.ioloop
+import main
 import os
 
 try:
-   zvirtenv = os.path.join(os.environ['OPENSHIFT_PYTHON_DIR'],
-                           'virtenv', 'bin', 'activate_this.py')
-   execfile(zvirtenv, dict(__file__ = zvirtenv) )
+    zvirtenv = os.path.join(os.environ['OPENSHIFT_PYTHON_DIR'], 'virtenv',
+                            'bin', 'activate_this.py')
+    execfile(zvirtenv, dict(__file__=zvirtenv))
 except IOError:
-   pass
+    pass
 
 if __name__ == '__main__':
-   ip   = os.environ['OPENSHIFT_PYTHON_IP']
-   port = int(os.environ['OPENSHIFT_PYTHON_PORT'])
-   app = imp.load_source('application', 'main.py')
+    ip = os.environ['OPENSHIFT_PYTHON_IP']
+    port = int(os.environ['OPENSHIFT_PYTHON_PORT'])
+    app = main.MainAPI()
 
-   app.application.listen(port , ip)
-   app.tornado.ioloop.IOLoop.instance().start()
+    app.application.listen(port, ip)
+    tornado.ioloop.IOLoop.current().start()
