e49d72ab360fc5714ce3b35b30351374c5e5b937
Sebastian Webber <sebastian@swebber.me>
2017-01-26 23:10:05 -0200

    updating tornado for wsgi stuff

--- app.py
+++ app.py
@@ -1,52 +1,14 @@
 #!/usr/bin/env python
 
-import os
-import tornado.ioloop
+import tornado.wsgi
 import tornado.web
-from generators import fdw, pgbadger
-from guides import native_replication
-from advisors import tuning
 
-
-class IndexHandler(tornado.web.RequestHandler):
-    def get(self):
-        self.render('index.html')
-
-
-class MainAPI():
-
-    def __init__(self):
-        self.settings = {
-            "template_path":
-            os.path.join(os.path.dirname(__file__), "templates"),
-            "static_path": os.path.join(os.path.dirname(__file__), "static"),
-            "debug": True,
-            "autoreload": True
-        }
-
-        API_VERSION = 1.0
-        API_PREFIX = "/v1/"
-
-        self.application = tornado.web.Application([
-            (r"/", tornado.web.RedirectHandler, {"url": "/v1"}),
-            (r"/v1", IndexHandler),
-
-            ## Generators
-            (r"/v1/generators/fdw/([^/]+)", fdw.FDWHandler),
-            (r"/v1/generators/pgbadger/([^/]+)", pgbadger.PGBadgerConfigurationHandler),
-
-            ## Guides
-            (r"/v1/guides/native-replication/([^/]+)", native_replication.NativeReplicationHandler),
-
-            ## Tuning
-            (r"/v1/tuning/([^/]+)", tuning.TuningHandler),
-        ], **self.settings)
-
-    def get_app(self):
-        return self.application
+import wsgiref.simple_server
+from main import MainAPI
 
 
 if __name__ == "__main__":
     apiApp = MainAPI()
-    apiApp.application.listen(5000)
-    tornado.ioloop.IOLoop.current().start()
+    wsgi_app = tornado.wsgi.WSGIAdapter(apiApp.application)
+    server = wsgiref.simple_server.make_server('', 8888, wsgi_app)
+    server.serve_forever()
