a8340af241741d52f39277045031e8bd98d04c3f
Sebastian Webber <sebastian@swebber.me>
2017-01-26 23:34:56 -0200

    updating app file

--- app.py
+++ app.py
@@ -1,14 +1,11 @@
-#!/usr/bin/env python
-
 import tornado.wsgi
 import tornado.web
 
-import wsgiref.simple_server
 from main import MainAPI
 
 
 if __name__ == "__main__":
     apiApp = MainAPI()
     wsgi_app = tornado.wsgi.WSGIAdapter(apiApp.application)
-    server = wsgiref.simple_server.make_server('', 8888, wsgi_app)
-    server.serve_forever()
+    # server = wsgiref.simple_server.make_server('', 8888, wsgi_app)
+    # server.serve_forever()
